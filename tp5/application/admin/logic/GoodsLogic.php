f03ee5bebfa24492b8354785ea57a3da4ce04a7c
woxihuannisja <1483943306@qq.com>
2018-12-14 19:34:29 +0800

    error page

--- tp5/application/admin/logic/GoodsLogic.php
+++ tp5/application/admin/logic/GoodsLogic.php
@@ -95,4 +95,123 @@ class GoodsLogic extends Model
             Db::name('goods_attr')->insertAll($data_attr);
         }
     }
+
+    /**
+     * 根据id查询商品的属性信息
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function getAttrInfo($id)
+    {
+        $goods_attrs=[];
+        $goods_attrs=Db::name('goods_attr')->field('attr_id,attr_value')->where('goods_id', $id)->select();
+        foreach ($goods_attrs as $key=>$attr) {
+            $attr_id=$attr['attr_id'];
+            $attribute=Db::name('attribute')->where('attr_id', $attr_id)->find();
+            // 获取属性名
+            $attr_name=$attribute['attr_name'];
+            $goods_attrs[$key]['attr_name']=$attr_name;
+        }
+        return $goods_attrs;
+    }
+
+    /**
+     * 根据商品id获取规格信息
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function getSpecInfo($id)
+    {
+        // 查询多规格商品的key 价格 库存等信息
+        $sql="SELECT * FROM ecs_spec_goods_price WHERE (goods_id=$id)";
+        $spec_goods_price=Db::query($sql);
+        if (empty($spec_goods_price)) {
+            return [];
+        }
+        // 规格名和规格项数据
+        $goods_spec=[];
+        // 当前激活的规格项key
+        $current_spec_key=[];
+
+      
+        $res=[];
+          
+        // 获取这种形式的key 20_24_26
+        // 将key的每个unit单元提取出来
+        foreach ($spec_goods_price as $key=>$value) {
+            $spec_key=$value['key'];
+            // 获取当前商品的key值
+            // 提取第一项作为激活的规格项组合
+            if (empty($current_spec_key)) {
+                $current_spec_key=explode('_', $spec_key);
+            }
+              
+            $res[]=str_replace('_', ",", $spec_key);
+        }
+        // 将所有的规格项id组合起来
+        $id_string=implode(',', $res);
+
+        // 从规格项表中查询所有的规格值 如黑色 白色
+        $sql="SELECT * FROM ecs_goods_spec_item WHERE (id in ($id_string))";
+        $goods_spec_item=Db::query($sql);
+
+        $id_string='';
+        foreach ($goods_spec_item as $key=>$value) {
+            $id_string.=$value['spec_id'];
+            if ($key <count($goods_spec_item)-1) {
+                $id_string.=',';
+            }
+        }
+
+        // 获取规格名称 如颜色 尺寸
+        $sql="SELECT * FROM ecs_goods_spec WHERE (id in ($id_string))";
+        $goods_spec=Db::query($sql);
+        
+
+        // 将规格名和对应的规格项联合起来
+        // 形成如下的结构：
+        // 颜色： 黑色 白色
+        // 尺寸：5寸 6寸
+        // 单位：件 包
+
+        // 形成如下的结构
+         //array(6)
+          // id:1
+          // type_id:4
+          // name:"颜色"
+          // sort:20
+          // is_show:1
+          // children:array(2)
+          // 0:array(3)
+          // id:1
+          // spec_id:1
+          // item:"白色"
+          // 1:array(3)
+          // id:2
+          // spec_id:1
+          // item:"黑色"
+        foreach ($goods_spec as $key=>$spec) {
+            $spec_id=$spec['id'];
+            // 遍历规格项
+            foreach ($goods_spec_item as $spec_item) {
+                if ($spec_id==$spec_item['spec_id']) {
+                    $goods_spec[$key]['children'][]=$spec_item;
+                }
+            }
+        }
+        return ['current_spec_key'=>$current_spec_key,'goods_spec'=>$goods_spec];
+    }
+
+    /**
+     * 根据商品id获取相册
+     *
+     * @param [type] $id
+     * @return void
+     */
+    public function getImagesInfo($id){
+        $images=Db::name('goods_images')->where('goods_id',$id)->select();
+        return $images;
+    }
 }
--- tp5/application/home/controller/Goods.php
+++ tp5/application/home/controller/Goods.php
@@ -4,6 +4,7 @@ namespace app\home\controller;
 use think\Controller;
 use think\View;
 use think\DB;
+use app\admin\logic\GoodsLogic;
 
 class Goods extends Controller
 {
@@ -28,105 +29,23 @@ class Goods extends Controller
         
         $goods_name=$goods['goods_name'];
        
-
+        $GoodsLogic=new GoodsLogic();
         // 查询商品的属性
-        $goods_attrs=[];
-        $goods_attrs=Db::name('goods_attr')->field('attr_id,attr_value')->where('goods_id', $id)->select();
-        foreach ($goods_attrs as $key=>$attr) {
-            $attr_id=$attr['attr_id'];
-            $attribute=Db::name('attribute')->where('attr_id', $attr_id)->find();
-            // 获取属性名
-            $attr_name=$attribute['attr_name'];
-            $goods_attrs[$key]['attr_name']=$attr_name;
-        }
+        $goods_attrs=$GoodsLogic->getAttrInfo($id);
 
-     
-        $goods_spec=[];
-        $current_spec_key=[];
-        // $p_id为0表示这是父级商品
-   
-        // 查询多规格商品的key 价格 库存等信息
-        $sql="SELECT * FROM ecs_spec_goods_price WHERE (goods_id=$id)";
-        $spec_goods_price=Db::query($sql);
+        // 查询商品的规格
+        $goods_spec_info=$GoodsLogic->getSpecInfo($id);
 
-        // 规格名和规格项数据
-        $goods_spec=[];
-        // 当前激活的规格项key
-        $current_spec_key=[];
-
-        if (!empty($spec_goods_price)) {
-            $res=[];
-            
-            // 获取这种形式的key 20_24_26
-            // 将key的每个unit单元提取出来
-            foreach ($spec_goods_price as $key=>$value) {
-                $spec_key=$value['key'];
-                // 获取当前商品的key值
-                // 提取第一项作为激活的规格项组合
-                if (empty($current_spec_key)) {
-                    $current_spec_key=explode('_', $spec_key);
-                }
-                
-                $res[]=str_replace('_', ",", $spec_key);
-            }
-            // 将所有的规格项id组合起来
-            $id_string=implode(',', $res);
-
-            // 从规格项表中查询所有的规格值 如黑色 白色
-            $sql="SELECT * FROM ecs_goods_spec_item WHERE (id in ($id_string))";
-            $goods_spec_item=Db::query($sql);
-
-            $id_string='';
-            foreach ($goods_spec_item as $key=>$value) {
-                $id_string.=$value['spec_id'];
-                if ($key <count($goods_spec_item)-1) {
-                    $id_string.=',';
-                }
-            }
-
-            // 获取规格名称 如颜色 尺寸
-            $sql="SELECT * FROM ecs_goods_spec WHERE (id in ($id_string))";
-            $goods_spec=Db::query($sql);
-          
-
-            // 将规格名和对应的规格项联合起来
-            // 形成如下的结构：
-            // 颜色： 黑色 白色
-            // 尺寸：5寸 6寸
-            // 单位：件 包
-            foreach ($goods_spec as $key=>$spec) {
-                $spec_id=$spec['id'];
-                // 遍历规格项
-                foreach ($goods_spec_item as $spec_item) {
-                    if ($spec_id==$spec_item['spec_id']) {
-                        $goods_spec[$key]['children'][]=$spec_item;
-                    }
-                }
-            }
-            //array(6)
-            // id:1
-            // type_id:4
-            // name:"颜色"
-            // sort:20
-            // is_show:1
-            // children:array(2)
-            // 0:array(3)
-            // id:1
-            // spec_id:1
-            // item:"白色"
-            // 1:array(3)
-            // id:2
-            // spec_id:1
-            // item:"黑色"
+        $goods_images=$GoodsLogic->getImagesInfo($id);
         
-      
-            // 商品通用信息
-            $this->assign('data', $goods);
-            // 商品规格信息
-            $this->assign('specData', $goods_spec);
-            $this->assign('spec_key', $current_spec_key);
-            $this->assign('attrs', $goods_attrs);
-            return $this->fetch('detail');
-        }
+        // 商品通用信息
+        $this->assign('data', $goods);
+        // 商品规格信息
+        $this->assign('specData', $goods_spec_info['goods_spec']);
+        $this->assign('spec_key', $goods_spec_info['current_spec_key']);
+        $this->assign('attrs', $goods_attrs);
+        $this->assign('images', $goods_images);
+        return $this->fetch('detail');
+        
     }
 }
